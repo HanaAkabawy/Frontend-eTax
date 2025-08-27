3954c69e742f63705275e259f767d6bcc6354e08
mahmoud2005ahmed <ma8935009@gmail.com>
2025-08-27 13:21:33 +0300

    fix issue in public middleware

--- src/Components/MiddleWare/PublicMiddleware.jsx
+++ src/Components/MiddleWare/PublicMiddleware.jsx
@@ -5,10 +5,10 @@ const PublicMiddleware = ({ children }) => {
   const token = localStorage.getItem('token'); // check if user is logged in
   const role = localStorage.getItem('role');   // get user role
 
-  if (token && role !== "admin") {
+  if (token && role && role !== "admin") {
     // Redirect logged-in non-admin users to their dashboard
     return <Navigate to="/" replace />;
-  } else if (token && role === "admin") {
+  } else if (token && role && role === "admin") {
     // Redirect admins to their dashboard
     return <Navigate to="/admin/dashboard" replace />;
   }
